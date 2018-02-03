72190c4721b25ad26c79d3f7bea8c09c3c896a9e
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-02-03 12:26:44 -0600

    Changed so when have, only shows candy I have, no 0's

--- CandyMarket/Program.cs
+++ CandyMarket/Program.cs
@@ -4,38 +4,39 @@ using System.Linq;
 
 namespace CandyMarket
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			// wanna be a l33t h@x0r? skip all this console menu nonsense and go with straight command line arguments. something like `candy-market add taffy "blueberry cheesecake" yesterday`
-			var db = SetupNewApp();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // wanna be a l33t h@x0r? skip all this console menu nonsense and go with straight command line arguments. something like `candy-market add taffy "blueberry cheesecake" yesterday`
+            var db = SetupNewApp();
 
-			var run = true;
-			while (run)
-			{
-				ConsoleKeyInfo userInput = MainMenu();
+            var run = true;
+            while (run)
+            {
+                ConsoleKeyInfo userInput = MainMenu();
 
-				switch (userInput.KeyChar)
-				{
-					case '0':
-						run = false;
-						break;
-					case '1': // add candy to your bag
+                switch (userInput.KeyChar)
+                {
+                    case '0':
+                        run = false;
+                        break;
+                    case '1': // add candy to your bag
 
-						// select a candy type
-						var selectedCandyType = AddNewCandyType(db);
+                        // select a candy type
+                        var selectedCandyType = AddNewCandyType(db);
 
-						/** MORE DIFFICULT DATA MODEL
+                        /** MORE DIFFICULT DATA MODEL
 						 * show a new menu to enter candy details
 						 * it would be convenient to show the menu in stages e.g. press enter to go to next detail stage, but write the whole screen again with responses populated so far.
 						 */
 
-						// if(moreDifficultDataModel) bug - this is passing candy type right now (which just increments in our DatabaseContext), but should also be passing candy details
-						db.SaveNewCandy(selectedCandyType.KeyChar);
-						break;
-					case '2':
-                        /** eat candy
+                        // if(moreDifficultDataModel) bug - this is passing candy type right now (which just increments in our DatabaseContext), but should also be passing candy details
+                        db.SaveNewCandy(selectedCandyType.KeyChar);
+                        break;
+                    case '2':
+                        EatCandy(db);
+                        /** 
 						 * select a candy type
 						 * 
 						 * select specific candy details to eat from list filtered to selected candy type
@@ -43,7 +44,7 @@ namespace CandyMarket
 						 * enjoy candy
 						 */
                         break;
-					case '3': //Show candy
+                    case '3': //Show candy
                         ShowCandy(db);
                         /** throw away candy
 						 * select a candy type
@@ -54,8 +55,8 @@ namespace CandyMarket
 						 * cry for lost candy
 						 */
                         break;
-					case '4':
-						/** give candy
+                    case '4':
+                        /** give candy
 						 * feel free to hardcode your users. no need to create a whole UI to register users.
 						 * no one is impressed by user registration unless it's just amazingly fast & simple
 						 * 
@@ -64,58 +65,58 @@ namespace CandyMarket
 						 * you'll need a way to select what user you're giving candy to.
 						 * one design suggestion would be to put candy "on the table" and then "give the candy on the table" to another user once you've selected all the candy to give away
 						 */
-						break;
-					case '5':
-						/** trade candy
+                        break;
+                    case '5':
+                        /** trade candy
 						 * this is the next logical step. who wants to just give away candy forever?
 						 */
-						break;
-					default: // what about requesting candy? like a wishlist. that would be cool.
-						break;
-				}
-			}
-		}
+                        break;
+                    default: // what about requesting candy? like a wishlist. that would be cool.
+                        break;
+                }
+            }
+        }
 
-		static DatabaseContext SetupNewApp()
-		{
-			Console.Title = "Cross Confectioneries Incorporated";
+        static DatabaseContext SetupNewApp()
+        {
+            Console.Title = "Cross Confectioneries Incorporated";
 
-			var cSharp = 554;
-			var db = new DatabaseContext(tone: cSharp);
+            var cSharp = 554;
+            var db = new DatabaseContext(tone: cSharp);
 
-			Console.SetWindowSize(60, 30);
-			Console.SetBufferSize(60, 30);
-			Console.BackgroundColor = ConsoleColor.White;
-			Console.ForegroundColor = ConsoleColor.Black;
-			return db;
-		}
+            Console.SetWindowSize(60, 30);
+            Console.SetBufferSize(60, 30);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            return db;
+        }
 
-		static ConsoleKeyInfo MainMenu()
-		{
-			View mainMenu = new View()
-					.AddMenuOption("Did you just get some new candy? Add it here.")
-					.AddMenuOption("Do you want to eat some candy? Take it here.")
+        static ConsoleKeyInfo MainMenu()
+        {
+            View mainMenu = new View()
+                    .AddMenuOption("Did you just get some new candy? Add it here.")
+                    .AddMenuOption("Do you want to eat some candy? Take it here.")
                     .AddMenuOption("Show me the candy options I have.")
-					.AddMenuText("Press 0 to exit.");
+                    .AddMenuText("Press 0 to exit.");
 
-			Console.Write(mainMenu.GetFullMenu());
-			ConsoleKeyInfo userOption = Console.ReadKey();
-			return userOption;
-		}
+            Console.Write(mainMenu.GetFullMenu());
+            ConsoleKeyInfo userOption = Console.ReadKey();
+            return userOption;
+        }
 
-		static ConsoleKeyInfo AddNewCandyType(DatabaseContext db)
-		{
-			var candyTypes = db.GetCandyTypes();
+        static ConsoleKeyInfo AddNewCandyType(DatabaseContext db)
+        {
+            var candyTypes = db.GetCandyTypes();
 
-			var newCandyMenu = new View()
-					.AddMenuText("What type of candy did you get?")
-					.AddMenuOptions(candyTypes);
+            var newCandyMenu = new View()
+                    .AddMenuText("What type of candy did you get?")
+                    .AddMenuOptions(candyTypes);
 
-			Console.Write(newCandyMenu.GetFullMenu());
+            Console.Write(newCandyMenu.GetFullMenu());
 
-			ConsoleKeyInfo selectedCandyType = Console.ReadKey();
-			return selectedCandyType;
-		}
+            ConsoleKeyInfo selectedCandyType = Console.ReadKey();
+            return selectedCandyType;
+        }
 
         static void ShowCandy(DatabaseContext db)
         {
@@ -123,12 +124,18 @@ namespace CandyMarket
             var haveCandyMenu = new View();
             foreach (var keyValuPair in FinalContents)
             {
-
-             
-                haveCandyMenu.AddMenuText($"{keyValuPair.Key} has {keyValuPair.Value} pieces of candy");
+                if (keyValuPair.Value >= 1)
+                {
+                    haveCandyMenu.AddMenuText($"{keyValuPair.Key} has {keyValuPair.Value} pieces of candy");
+                }
             }
             Console.Write(haveCandyMenu.GetFullMenu());
             Console.ReadKey();
+        }
+
+        static void EatCandy(DatabaseContext db)
+        {
+            
         }
 	}
 }
